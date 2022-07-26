92c9cd7b68719e8fc46b85f231ec14b4ed4ef281
faculeveratto <faculeve2492@gmail.com>
2022-07-26 14:38:09 -0300

    cambios 8

--- api/index.js
+++ api/index.js
@@ -20,9 +20,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
